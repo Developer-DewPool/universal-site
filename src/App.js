b0544a28c87a5d97455f5b99ca556036a31e5c52
dipankar chowhdury <dipankarchoudhury.28.dc@gmail.com>
2021-08-09 15:28:00 +0530

    intial push to new branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import PrivateRoute from './containers/Utils/PrivateRoute';
+import PublicRoute from './containers/Utils/PublicRoute';
+import Login from './containers/LoginComponent/Login';
+import Dashboard from './containers/DashboardComponent/Dashboard';
+import Home from './containers/HomeComponent/Home';
+import { getToken, removeUserSession, setUserSession } from './containers/Utils/Common';
+
 
 function App() {
+  const [authLoading, setAuthLoading] = useState(true);
+
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      return;
+    }
+
+    axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(response => {
+      setUserSession(response.data.token, response.data.user);
+      setAuthLoading(false);
+    }).catch(error => {
+      removeUserSession();
+      setAuthLoading(false);
+    });
+  }, []);
+
+  if (authLoading && getToken()) {
+    return <div className="content">Checking Authentication...</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink> {/*<small>(Access without token only)</small>*/}
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink>{/*<small>(Access with token only)</small>*/}
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <PublicRoute path="/login" component={Login} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
